5a497ccdfcbfdc1b375f355723e930567757eabe
Simon Burkhardt <simon@simonmartin.ch>
2024-01-17 07:13:45 +0100

    further experiments

--- ghdl/basic/skidbuffer-myversion.vhd
+++ ghdl/basic/skidbuffer-myversion.vhd
@@ -74,7 +74,11 @@ architecture behav of skidbuffer is
   signal o_ready : std_logic := '0';
   signal i_ready : std_logic := '0';
 
+  signal s_valid_reg : std_logic := '0';
   signal en_ready : std_logic := '0';
+
+  signal o_data_reg : std_logic_vector(DATA_WIDTH - 1 downto 0) := (others => '0');
+  signal o_valid_reg : std_logic := '0';
 
 begin
   p_reg : process(s_aclk) begin
@@ -82,9 +86,11 @@ begin
       if s_aresetn = '0' then
         s_data_reg <= (others => '0');
         o_ready <= '0';
+        s_valid_reg <= '0';
       else
         if o_ready = '1' then
           s_data_reg <= s_data;
+          s_valid_reg <= s_valid;
         end if;
         if en_ready = '1' then
           o_ready <= i_ready;
@@ -97,12 +103,35 @@ begin
   
   en_ready <= s_valid OR (NOT o_ready);
   o_data <= s_data when o_ready = '1' else s_data_reg;
-  o_valid <= en_ready;
+  o_valid <= s_valid when o_ready = '1' else s_valid_reg;
  
-
+  gen_no_out_register : if not OPT_DATA_REG generate
     m_valid <= o_valid;
     m_data <= o_data;
     i_ready <= m_ready;
+  end generate;
+
+  gen_out_register : if OPT_DATA_REG generate
+
+    p_out_reg : process(s_aclk) begin
+      if rising_edge(s_aclk) then
+        if s_aresetn = '0' then
+          o_valid_reg <= '0';
+          o_data_reg <= (others => '0');
+        else
+          if i_ready = '1' then
+            o_valid_reg <= o_valid;
+            o_data_reg <= o_data;
+          end if;
+        end if;
+      end if;
+    end process;
+
+    i_ready <= m_ready OR (NOT o_valid_reg);
+    m_data <= o_data_reg;
+    m_valid <= o_valid_reg;
+
+  end generate;
 
 
 end behav;
